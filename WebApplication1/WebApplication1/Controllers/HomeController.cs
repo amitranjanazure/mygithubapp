d016f51abbbd2a362289fcc862541c4644f0b124
Amit Ranjan <amitranjan.ss@smartdatainc.net>
2020-08-26 13:50:45 +0530

    test function

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace WebApplication1.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
             //amit
+
+        }
+        private void test()
+        {
+            //new function
         }
     }
 }
